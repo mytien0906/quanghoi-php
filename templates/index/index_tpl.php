c9196947feaa6a57008bd1e6506835454bd57d85
mytien <thaitiendrm@gmail.com>
2022-03-08 15:28:24 +0700

    up form

--- templates/index/index_tpl.php
+++ templates/index/index_tpl.php
@@ -57,7 +57,74 @@
 		</div>
 	</div>
 </div> -->
+<div class="wrap-contact">
+	<div class="container-fluid">
+		<div class="row">
+			<div class="col-lg-6 col-md-6 col-sm-6 col-12 py-5" style="padding-left: 4rem;">
+				<div class="wrap-contact-left">
+					<form class="form-contact validation-contact" novalidate="" method="post" action="" enctype="multipart/form-data">
+						<div class="form-register-news-left-title">
+							<h1>Đăng ký nhận tin</h1>
+							<p>Điền thông tin của bạn vào form bên dưới để đăng ký nhận báo giá từ chúng tôi</p>
+						</div>
+						<div class="row" style="padding-top: 16px">
+							<div class="input-contact col-sm-6">
+								<input type="text" class="form-control" id="ten" name="ten" placeholder="" required="">
+								<div class="underline"></div>
+								<label for="">Họ và tên:</label>
+								<div class="invalid-feedback">Vui lòng nhập họ và tên</div>
+							</div>
+							<div class="input-contact col-sm-6">
+								<input type="number" class="form-control" id="dienthoai" name="dienthoai" placeholder="" required="">
+								<div class="underline"></div>
+								<label for="">Số điện thoại:</label>
+								<div class="invalid-feedback">Vui lòng nhập số điện thoại</div>
+							</div>
+						</div>
+						<div class="row">
 
+							<div class="input-contact col-sm-6">
+								<input type="email" class="form-control" id="email" name="email" placeholder="" required="">
+								<div class="underline"></div>
+								<label for="">Email:</label>
+								<div class="invalid-feedback">Vui lòng nhập địa chỉ email</div>
+							</div>
+							<div class="input-contact col-sm-6">
+								<input type="text" class="form-control" id="diachi" name="diachi" placeholder="" required="">
+								<div class="underline"></div>
+								<label for="">Địa chỉ:</label>
+								<div class="invalid-feedback">Vui lòng nhập địa chỉ</div>
+							</div>
+						</div>
+						<div class="input-contact">
+							<input type="text" class="form-control" id="tieude" name="tieude" placeholder="" required="">
+							<div class="underline"></div>
+							<label for="">Chủ đề</label>
+							<div class="invalid-feedback">Vui lòng nhập chủ đề</div>
+						</div>
+						<div class="input-contact">
+							<input type="hidden" class="form-control" id="tieude" name="tieude" placeholder="" required="">
+							<div class="underline"></div>
+							<label for="">Nội dung</label>
+							<div class="invalid-feedback">Vui lòng nhập chủ đề</div>
+						</div>
+						<div class="input-contact">
+							<textarea class="form-control" id="noidung" name="noidung" placeholder="" required=""></textarea>
+							<div class="invalid-feedback">Vui lòng nhập nội dung</div>
+						</div>
+						<input type="submit" class="btn btn-primary" name="submit-contact" value="Gửi">
+						<input type="hidden" name="recaptcha_response_contact" id="recaptchaResponseContact" value="HFbGx2fxVRejkAYWxDQkZSQwAeb34AZR8CEWIQDyoCFxEnJWg5MUk6PDxCSzZWLG0jDTVQCB8FRVlLRnhyaQMGaAB8e0x2SEpUYHorKG9_Zm94EwwGTGAgZQV1bAgFHwgFZhN1aXdZVnlpDWJPdWd0Y2Q9GWR0MnVdN18VFgUhbS4HQw">
+					</form>
+				</div>
+			</div>
+			<div class="col-lg-6 col-md-6 col-sm-6 col-12">
+				<div class="wrap-contact-right">
+					<iframe src="https://www.youtube.com/embed/Akhn15oT-bw" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen=""></iframe>
+				</div>
+			</div>
+		</div>
+	</div>
+</div>
 <div class="wrap-news">
 	<div class="fixwidth">
 		<div class="title">Tin tức mới nhất</div>
