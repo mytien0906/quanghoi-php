29889b25c121db0420e722ffda847a3fa0bb8520
mytien <thaitiendrm@gmail.com>
2022-03-09 15:06:29 +0700

    update css

--- templates/layout/menu.php
+++ templates/layout/menu.php
@@ -13,14 +13,14 @@
             <div class="top-header">
                 <div class="container">
                     <div class="row">
-                        <div class="col-md-6 left-top-header">
+                        <div class="col-md-7 col-sm-7 col-12 left-top-header">
                             <p><svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 384 512" id="svg-map">
                                     <!--! Font Awesome Pro 6.0.0 by @fontawesome - https://fontawesome.com License - https://fontawesome.com/license (Commercial License) Copyright 2022 Fonticons, Inc. -->
                                     <path
                                         d="M168.3 499.2C116.1 435 0 279.4 0 192C0 85.96 85.96 0 192 0C298 0 384 85.96 384 192C384 279.4 267 435 215.7 499.2C203.4 514.5 180.6 514.5 168.3 499.2H168.3zM192 256C227.3 256 256 227.3 256 192C256 156.7 227.3 128 192 128C156.7 128 128 156.7 128 192C128 227.3 156.7 256 192 256z" />
                                 </svg> Địa chỉ: 29/8/25A Bình Đông, P.15, Quận 8,Tp Hồ Chí Minh</p>
                         </div>
-                        <div class="col-md-6 right-top-header">
+                        <div class="col-md-5 col-sm-5 col-12 right-top-header">
 
                             <div class="social2">
                                 <span>Liên kết MXH: </span>
